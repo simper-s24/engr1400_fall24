424d02113ee55e0ffdaa96b310d767a2cef652d9
Dallan Simper <dallan.simper@gmail.com>
2024-10-14 15:20:35 -0600

    working with setw

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,48 +1,20 @@
 #include <iostream>
-#include<string>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
-void ArraySearch(int data[], int count, int target);
 
 int main() {
-	
-	int i = 45;
-	cout << "i = " << i << endl;
-	double d = i;
-	cout << "d = " << d << endl;
-
-	int a = 12;
-	int b = 5;
-
-	cout << static_cast<double>(a) / b << endl;
+	double housecost = 2500000.0;
+	double rate = 0.056555454;
 
 
-
-
-	double f = 10 / 4;
-
-	int j = '5';
-
-	int count = 10;
-	int input[] = {3, 4, 76, 43, 101, 5, 1, 0, 7, 12};
-
-	ArraySearch(input, count, 34);
-	ArraySearch(input, count, 12);
-	ArraySearch(input, count, 56);
-
-
-	string value = "this is what we do";
-
-	//getline(cin, value);
-
-	for (int i = 0; i < value.size(); i++) {
-		if (value[i] > 'a' && value[i] < 'm') {
-			cout << value[i];
-		}
+	cout << " Rate   Total Interest" << endl;
+	for (double i = 0; i < 0.1;i = i + 0.01) {
+		cout << setprecision(2) << fixed;
+		cout << setw(6) << (rate + i) * 100 << "% " << setw(13) << (rate + i) * housecost * 30 << endl;
 	}
+
 }
 
-void ArraySearch(int data[], int count, int target)
-{
-}
