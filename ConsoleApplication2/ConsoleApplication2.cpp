847621456f6a2f48dab3ec2259d1cfcdab9c9eab
Dallan Simper <dallan.simper@gmail.com>
2024-10-16 15:21:27 -0600

    switch case

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -5,16 +5,71 @@
 using namespace std;
 
 
+void foo() {
+	char choice = 'r';
+}
+
+
 int main() {
-	double housecost = 2500000.0;
-	double rate = 0.056555454;
+
+	char choice;
+
+	int list[] = { 56,8,7,8,4,2,4,5,6 };
 
 
-	cout << " Rate   Total Interest" << endl;
-	for (double i = 0; i < 0.1;i = i + 0.01) {
-		cout << setprecision(2) << fixed;
-		cout << setw(6) << (rate + i) * 100 << "% " << setw(13) << (rate + i) * housecost * 30 << endl;
+	bool isRunning = true;
+	while (isRunning) {
+		cout << "What do you want to do: ";
+		cin >> choice;
+
+		bool first = true;
+
+		switch (choice) {
+		case 'A':
+		case 'a':
+			cout << "You typed an a" << endl;
+			break;
+		case 'd':
+			cout << "You typed a d" << endl;
+			foo();
+			break;
+		case '3':
+			int accumulator;
+			for (int i = 0; i < 9; i++) {
+				accumulator = i;
+				cout << list[i];
+				if (i != 9 - 1) {
+					cout << ", ";
+				}
+			}
+			cout << accumulator;
+			cout << endl;
+			break;
+		case 'q':
+			cout << "You typed a q" << endl;
+			isRunning = false;
+			break;
+		default:
+			cout << "I don't know that command" << endl;
+			break;
+		}
 	}
 
+	/*
+	bool isEndOfAlphabet = choice >= 's' && choice <= 'w';
+
+	if (choice == 'a' || choice == 'A')
+		cout << "You type 'a' or 'A'" << endl;
+	else if (choice == 1)
+		cout << "You typed a 1" << endl;
+	else if (choice == 2)
+		cout << "You typed a 2" << endl;
+	else if (choice == 3)
+		cout << "You typed a 3" << endl;
+	else if (isEndOfAlphabet)
+		cout << "You typed an 's' to 'w'" << endl;
+	else
+		cout << "I don't know that command" << endl;
+	//*/
 }
 
