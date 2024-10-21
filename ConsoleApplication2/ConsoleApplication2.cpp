6bda534372358be05f414cad82cf346df2e19aac
Dallan Simper <dallan.simper@gmail.com>
2024-10-21 15:20:13 -0600

    file input

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,40 +1,29 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
 
-void foo() {
-	char choice = 'r';
-	int sandwich = 57;
-}
 
 
 int main() {
 
-	//sandwich++;
+	ifstream file;
 	
-	for (int index = 0; index < 5; index++)
-		cout << index << " ";
-	//cout << index << " ";
-	cout << endl;
+	file.open("Class.txt");
 
-	cout << "How many points in each game?\n";
-	int list[100];
-	int i = 0;
-	do  {
-		cin >> list[i];
-		i++;
-	} while (list[i - 1] > 0);
-	list[i - 1] = -1;
+	string name;
 
-	i = 0;
-	int acc = 0;
-	while (list[i] != -1) {
-		acc += list[i];
+	int i = 1;
+	//while (getline(file, name)) {
+	while (file >> name) {
+		
+		cout << "Person " << i << ": " << name << endl;
+
 		i++;
 	}
-	cout << "Total points: " << acc;
+
 }
 
