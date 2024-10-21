031a54e0c050e7571ad9ef0acc7b18a720662daa
Dallan Simper <dallan.simper@gmail.com>
2024-10-21 14:59:46 -0600

    scope and sentibal

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -7,69 +7,34 @@ using namespace std;
 
 void foo() {
 	char choice = 'r';
+	int sandwich = 57;
 }
 
 
 int main() {
 
-	char choice;
+	//sandwich++;
+	
+	for (int index = 0; index < 5; index++)
+		cout << index << " ";
+	//cout << index << " ";
+	cout << endl;
 
-	int list[] = { 56,8,7,8,4,2,4,5,6 };
+	cout << "How many points in each game?\n";
+	int list[100];
+	int i = 0;
+	do  {
+		cin >> list[i];
+		i++;
+	} while (list[i - 1] > 0);
+	list[i - 1] = -1;
 
-
-	bool isRunning = true;
-	while (isRunning) {
-		cout << "What do you want to do: ";
-		cin >> choice;
-
-		bool first = true;
-
-		switch (choice) {
-		case 'A':
-		case 'a':
-			cout << "You typed an a" << endl;
-			break;
-		case 'd':
-			cout << "You typed a d" << endl;
-			foo();
-			break;
-		case '3':
-			int accumulator;
-			for (int i = 0; i < 9; i++) {
-				accumulator = i;
-				cout << list[i];
-				if (i != 9 - 1) {
-					cout << ", ";
-				}
-			}
-			cout << accumulator;
-			cout << endl;
-			break;
-		case 'q':
-			cout << "You typed a q" << endl;
-			isRunning = false;
-			break;
-		default:
-			cout << "I don't know that command" << endl;
-			break;
-		}
+	i = 0;
+	int acc = 0;
+	while (list[i] != -1) {
+		acc += list[i];
+		i++;
 	}
-
-	/*
-	bool isEndOfAlphabet = choice >= 's' && choice <= 'w';
-
-	if (choice == 'a' || choice == 'A')
-		cout << "You type 'a' or 'A'" << endl;
-	else if (choice == 1)
-		cout << "You typed a 1" << endl;
-	else if (choice == 2)
-		cout << "You typed a 2" << endl;
-	else if (choice == 3)
-		cout << "You typed a 3" << endl;
-	else if (isEndOfAlphabet)
-		cout << "You typed an 's' to 'w'" << endl;
-	else
-		cout << "I don't know that command" << endl;
-	//*/
+	cout << "Total points: " << acc;
 }
 
