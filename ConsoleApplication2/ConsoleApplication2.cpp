2c5b5b93847b024086088dd099b2d2e0d7d795dd
Dallan Simper <dallan.simper@gmail.com>
2024-11-08 15:01:09 -0700

    Vector

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iomanip>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -17,16 +18,46 @@ int foo(int a) {
 }
 
 
+void printVector(vector<int> items) {
+	for (int j = 0; j < items.size(); j++) {
+		cout << items[j] << ", ";
+	}
+	cout << endl;
+}
+
 
 int main() {
-	cout << "d: " << d << endl;
 
-	int num = foo(5); cout << "num: " << num << endl;
-	num = foo(5); cout << "num: " << num << endl;
-	num = foo(5); cout << "num: " << num << endl;
-	num = foo(5); cout << "num: " << num << endl;
+	int array[20] = { 4,5,6,45,65,12 };
+	vector<int> list = { 4,5,6,45,65,12 };
+
+	//vector<int> list(100);
+	//list[67] = 12;
+
+	for (int i = 0; i < 10; i++)
+	{
+		list.push_back(i);
+		array[i + 5] = i;
+	}
+
+	printVector(list);
 	
-	cout << "d: " << d << endl;
+	int first = (list.size() > 0 ? list[0] : -1);
+	
+	list.erase(list.begin() + 0);
+	printVector(list);
 
+	int last = list[list.size() - 1];
+	list.pop_back();
+	printVector(list);
+
+	int acc = 0; 
+	for (int j = 0; j < list.size(); j++) {
+		acc += list[j];
+	}
+	//cout<< endl << acc;
+
+	cout << "First: " << first << endl;
+	cout << "Last: " << last << endl;
 }
 
