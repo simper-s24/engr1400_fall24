a591f727096fd2ee41bb88d49f1a0caba2a0dcc4
Dallan Simper <dallan.simper@gmail.com>
2024-11-18 15:24:55 -0700

    pointers

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -7,96 +7,44 @@
 
 using namespace std;
 
-struct Point {
-	int x;
-	int y;
-};
-
-struct Point3d {
-	Point flat;
-	int z;
-};
-
-struct IntList {
-	int data[6];
-};
-
-
-int d = 6;
-
-int foo(int a) {
-	int b = a;
-	static int c = 0;
-	c += b;
-	b *= 11;
-	d *= b;
-	return c;
+int foo(int* ptr) {
+	(*ptr)++;
+	return *ptr + 1;
 }
 
-
-void printVector(vector<int> items) {
-	for (int j = 0; j < items.size(); j++) {
-		cout << items[j] << ", ";
-	}
-	cout << endl;
+int bar(int n) {
+	n++;
+	return n + 1;
 }
-
-
-int bar(int list[][4][3]) {
-
-	return 0;
-}
-
-int myFunction() {  }
 
 int main() {
+	int num = 7;
+	int* p = &num;
 
-	int num = 3.45;
-	int arr[][7] = { 3,4,5,6,5,5 };
+	//*
+	int b = foo(p);
+	cout << num << endl;
+	cout << b << endl;
+	//*/
 
- 
-	Point here = { 1,2 };
-	Point there;
-	there.x = 7;
-	there.y = 12;
+	/*
+	int b = bar(num);
+	cout << num << endl;
+	cout << b << endl;
+	//*/
 
-	Point3d space;
-	space.flat.x = 37;
-	space.z = 62;
+	int* r = nullptr;
+	r = new int[num];
 
-	vector<Point> points;
-	vector<string> names = { "Cooper", "Kenyon" };
-
-	int array[20] = { 4,5,6,45,65,12 };
-	vector<int> list = { 4,5,6,45,65,12 };
-
-	//vector<int> list(100);
-	//list[67] = 12;
-
-	for (int i = 0; i < 10; i++)
-	{
-		list.push_back(i);
-		array[i + 5] = i;
+	for (int i = 0; i < num; i++) {
+		r[i] = i;
+		*(r + i) = r[i] + i;
 	}
 
-	printVector(list);
-	
-	int first = (list.size() > 0 ? list.at(0) : -1);
-	
-	list.erase(list.begin() + 0);
-	printVector(list);
-
-	int last = list[list.size() - 1];
-	list.pop_back();
-	printVector(list);
-
-	int acc = 0; 
-	for (int j = 0; j < list.size(); j++) {
-		acc += list[j];
+	for (int i = 0; i < num; i++) {
+		cout << *(r + i) << ", ";
 	}
-	//cout<< endl << acc;
-
-	cout << "First: " << first << endl;
-	cout << "Last: " << last << endl;
+	cout << r << endl;
+	cout << &r << endl;
 }
 
