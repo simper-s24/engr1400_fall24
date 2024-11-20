a814647557814d267b9347793e6b308a7d3f1dcc
Dallan Simper <dallan.simper@gmail.com>
2024-11-20 16:56:36 -0700

    more pointers

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -7,54 +7,35 @@
 
 using namespace std;
 
-int foo(int* ptr) {
-	(*ptr)++;
-	return *ptr + 1;
+int foo(vector<int> list2) {
+	int sum = 0;
+	for (int i = 0; i < list2.size(); i++)
+		sum += list2[i];
+	cout << "list2 address: " << &list2  << endl;
+	return sum;
 }
 
-int bar(int n) {
-	n++;
-	return n + 1;
+int bar(vector<int>* pv) {
+	int sum = 0;
+	cout << "*pv address: " << &pv << "; target address: " << pv << endl;
+	for (int i = 0; i < (*pv).size(); i++)
+		sum += (*pv)[i];
+	return sum;
 }
-
 
 int main() {
+	vector<int> list;
+	vector<int>* ptr = nullptr;
+	ptr = &list;
 
+	list.push_back(7);
+	list.push_back(73);
+	
+	cout << "list address: " << &list << endl;
+	cout << "*ptr address: " << &ptr << "; target address: " << ptr << endl;
 
-	int num = 7;
-	int* p = &num;
+	foo(list);
+	bar(ptr);
 
-	cout << "num: " << num << "; address : " << &num << endl;
-	cout << "*p: " << p << "; address : " << &p << "; target value: " << *p <<  endl;
-
-	//*
-	int b = foo(p);
-	cout << num << endl;
-	cout << b << endl;
-	//*/
-
-	//*
-	int b1 = bar(num);
-	cout << num << endl;
-	cout << b1 << endl;
-	//*/
-	cout << "b: " << b << "; address : " << &b << endl;
-	cout << "b1: " << b1 << "; address : " << &b1 << endl;
-
-
-	/*int* r = nullptr;
-	r = new int[num];
-
-	for (int i = 0; i < num; i++) {
-		r[i] = i;
-		*(r + i) = r[i] + i;
-	}
-
-	for (int i = 0; i < num; i++) {
-		cout << *(r + i) << ", ";
-	}
-	cout << endl;
-	cout << r << endl;
-	cout << &r << endl;*/
 }
 
